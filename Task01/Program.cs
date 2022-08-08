7fd6aef9b862d2e32a3c5f3f5f893a4bef4ef6c6
Yulia <yulia1201556@gmail.com>
2022-08-08 03:51:33 +0300

    Число и его квадрат

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет.
+
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num2 == num * num)
+{
+Console.Write($"Квадрат числа {num} = {num2}");
+}
+else
+{
+    Console.Write($"Квадрат числа {num} не является число{num2}");
+}
