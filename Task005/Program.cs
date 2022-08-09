7ac9fc37dd357b7092017a68721192afe59aedfc
Yulia <yulia1201556@gmail.com>
2022-08-09 18:01:44 +0300

    Наибольшее число, наибольшая цифра в нем

---
+++ Task005/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit == secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра из числа {number} -> Цифры одинаковые");
+}
+else if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра из числа {number} -> {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра из числа {number} -> {secondDigit}");
+}
+
+
